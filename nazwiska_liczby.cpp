62e79b80ea9ca3928e3715a577c20a021328ea40
Adrian Karwowski <adrian_karwowski1@wp.pl>
2017-03-22 16:40:43 +0100

    scalone2

--- nazwiska_liczby.cpp
+++ nazwiska_liczby.cpp
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+#include <iostream>
 
-void main( void )
+using namespace std;
+
+int znajdzNaj( int poprzednia )
 {
+   ifstream liczby("liczby.txt");
+
+   int liczba;
+    int najwieksza = 0;
+   while( !liczby.eof() )
+   {
+       liczby >> liczba;
+       //cout << liczba << endl;
+       if( liczba > najwieksza && liczba < poprzednia )
+{
+            najwieksza = liczba;
+        //cout << najwieksza << endl;
+}
+   }
+    cout << najwieksza << endl;
+
+    return najwieksza;
+}
+
+void sortujMal()
+{
+    ofstream liczby_mal("liczby_mal.txt");
+
+    int poprzednia = 9999;
+
+    for(int i = 0; i < 28; ++i )
+    {
+       poprzednia = znajdzNaj(poprzednia);
+       liczby_mal << poprzednia << endl;
+    }
+}
+int main()
+{
+	sortujMal();
   FILE *plik;
   char strtmp[ 501 ];
   char nazwisko[ 501 ];
@@ -58,5 +96,5 @@ void main( void )
   }
   fclose( plik );
 
-  return;
+  return 0;
 }
